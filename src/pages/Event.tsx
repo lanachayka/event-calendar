3b7a295ea5991d7690e54e83c77e5bfb41b872f6
Руслана Підсадюк <ruslana.chaikivska@gail.com>
2022-05-11 13:21:03 +0300

    initial commit

---
+++ src/pages/Event.tsx
@@ -0,0 +1,5 @@
+const Event: React.FC = () => {
+    return <div>EVENT</div>
+}
+
+export default Event
---
+++ src/pages/Login.tsx
@@ -0,0 +1,5 @@
+const Login: React.FC = () => {
+    return <div>LOGIN</div>
+}
+
+export default Login
